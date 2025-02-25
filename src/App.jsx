5cfca9ac31665c4a1f534711257b9c4a4ce0ac8e
aa <aaaa>
2025-02-25 22:27:24 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+// function App() {
+//   const [count, setCount] = useState(0)
+const App = () => {
   const [count, setCount] = useState(0)
+
+  const myFunction = () => {
+    console.log(">>> run my arrow function")
+  }
+
+  myFunction();
 
   return (
     <>
