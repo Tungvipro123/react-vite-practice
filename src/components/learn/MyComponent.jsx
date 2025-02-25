73c8e426e7cff0de07efcbb9d7e432b9e17a20af
aa <aaaa>
2025-02-25 23:24:07 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,18 @@
 
 //() => {}
 //component = html + css + js
+
+//JSX
+//fragment
+import './style.css';
 const MyComponent = () => {
     return (
-        <div> eric & hoidanit update </div>
+        <>
+            <div> eric & hoidanit update </div>
+            <div className="child"
+                style={{ borderRadius: "10px" }}
+            >child</div>
+        </>
     );
 }
 
