0a9b40c90bd12c86152d7edeb94af297f0b49c60
aa <aaaa>
2025-02-25 23:10:13 +0700

    video 29

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-//() => {}
-//component = html + css + js
-const MyComponent = () => {
-  return (
-    <div> eric & hoidanit update </div>
-  );
-}
+import MyComponent from './components/learn/MyComponent'
+import SecondComponent from './components/learn/SecondComponent.jsx'
 
 const App = () => {
   const [count, setCount] = useState(0)
@@ -26,6 +20,7 @@ const App = () => {
       </div>
       <h1>Hello World</h1>
       <MyComponent />
+      <SecondComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
