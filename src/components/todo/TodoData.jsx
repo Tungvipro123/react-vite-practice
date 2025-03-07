fc5b691bc48242e63246aa7fd9696a0ce1e276c1
aa <aaaa>
2025-03-07 23:39:59 +0700

    video 34

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,7 +1,23 @@
 
-const TodoData = () => {
+const TodoData = (props) => {  //tham so dau vao props de su dung cac props da truyen
+    // props la 1 bien object {}
+    // {
+    //     name: "Eric",
+    //     age: 25,
+    //     data: {}
+    // }
+    const { name, age, data } = props;    //doc 34 destructuring data
+
+    // const name = props.name;
+    // const age = props.age;
+    // const data = props.data;
+
+
+    console.log(">>> check props: ", props)
     return (
         <div className='todo-data'>
+            {/* <div> My name is {props.name}</div> */}
+            <div> My name is {name}</div>
             <div> Learning React</div>
             <div> Watching Youtube</div>
         </div>
