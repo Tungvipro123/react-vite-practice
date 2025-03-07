79f2528c982b5c2caf9832500f1c825d23b23d09
aa <aaaa>
2025-03-07 22:02:54 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,14 +3,28 @@
 //component = html + css + js
 
 //JSX
-//fragment
+//fragment <> </>
 import './style.css';
 const MyComponent = () => {
+    // const hoidanit = "eric 1"; //string              // kieu so va kieu chuoi
+    // const hoidanit = 25; //number
+    // const hoidanit = true; //boolean
+    // const hoidanit = undefine;
+    // const hoidanit = null;
+
+    const hoidanit = [1, 2, 3]
+    // const hoidanit = {       // su dung {JSON.stringify(hoidanit)} de bien object thanh string
+    //     name: "hoidanit",
+    //     age: 25
+    // }
     return (
         <>
-            <div> eric & hoidanit update </div>
+            <div> {JSON.stringify(hoidanit)} & hoidanit update </div>
+            <div>{console.log("ERIC")}</div>
             <div className="child"
-                style={{ borderRadius: "10px" }}
+                style={     // dau nhay the hien su dung JS trong HTML
+                    { borderRadius: "10px" }        // dau nhay tuong trung cho Object
+                }
             >child</div>
         </>
     );
